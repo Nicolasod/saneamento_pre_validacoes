e001f408399dc5b40562fbfbc9afcd2539ee7c62
Nicolas Durante <nicolasouriquesdurante@gmail.com>
2025-07-17 10:45:07 -0300

    Ajuste da validação 2: correção na atualização das descrições duplicadas na tabela turmas

--- PONTO - Validação - 2.sql
+++ PONTO - Validação - 2.sql
@@ -9,6 +9,14 @@ select list(i_entidades) as entidades,
  group by descricao 
 having quantidade > 1;
 
-
 -- CORREÇÃO
-
+update bethadba.turmas t
+   set descricao = t.i_turmas || '-' || t.descricao
+ where exists (
+   select 1
+     from bethadba.turmas t2
+    where trim(t2.descricao) = trim(t.descricao)
+    group by trim(t2.descricao)
+    having count(*) > 1
+ );
+--
