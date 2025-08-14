38aa1d580696975c2d766dbcc913cac1eb0f33bd
Nicolas Durante <nicolasouriquesdurante@gmail.com>
2025-08-14 16:19:21 -0300

    Att nas validações: 11, 127, 141, 153

--- FOLHA - Validação - 127.sql
+++ FOLHA - Validação - 127.sql
@@ -12,6 +12,13 @@ select rc.campo
 -- CORREÇÃO
 -- Atualiza os campos CNPJ que estão nulos para 0 para que não sejam considerados na validação e não gere erro de validação.
 
-update bethadba.bethadba.rais_campos
-   set CNPJ = right('000000000000' || cast((row_number() over) as varchar(12)), 12) || '91'
- where CNPJ is null;
+update bethadba.rais_campos
+   set cnpj = '000000000000'
+ where cnpj is null
+   and exists (
+       select 1
+         from bethadba.rais_eventos re
+        where re.campo = rais_campos.campo
+   );
+
+commit;
